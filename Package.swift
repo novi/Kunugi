e58c53d59debd493dc8f41fef38391fac357c3df
Yusuke Ito <novi.mad@gmail.com>
2016-01-04 19:52:52 +0900

    fix `swift build` error

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Kunugi",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0)
+        //.Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 9),
+		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
     ]
 )
