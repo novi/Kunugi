1434be9e63679ede36e53f2e33fd6abf68a69b1c
Yusuke Ito <novi.mad@gmail.com>
2016-01-09 16:53:57 +0900

    implement before and after hook functions in Controller

--- Sources/Controller.swift
+++ Sources/Controller.swift
@@ -12,21 +12,37 @@ public protocol ControllerMiddleware: MiddlewareType {
     func post(ctx: ContextBox) throws -> MiddlewareResult
     func put(ctx: ContextBox) throws -> MiddlewareResult
     func delete(ctx: ContextBox) throws -> MiddlewareResult
+    func before(ctx: ContextBox) throws -> MiddlewareResult
+    func after(ctx: ContextBox, result: MiddlewareResult) throws -> MiddlewareResult
 }
 
 public extension ControllerMiddleware {
+    func before(ctx: ContextBox) throws -> MiddlewareResult {
+        return .Next
+    }
+    func after(ctx: ContextBox, result: MiddlewareResult) throws -> MiddlewareResult {
+        return result
+    }
     func handle(ctx: ContextBox) throws -> MiddlewareResult {
-        switch ctx.request.method {
-        case .GET:
-            return try get(ctx)
-        case .POST:
-            return try post(ctx)
-        case .PUT:
-            return try put(ctx)
-        case .DELETE:
-            return try delete(ctx)
-        default:
-            return .Next
+        switch try before(ctx) {
+        case .Next:
+            let result: MiddlewareResult
+            switch ctx.request.method {
+            case .GET:
+                result = try get(ctx)
+            case .POST:
+                result = try post(ctx)
+            case .PUT:
+                result = try put(ctx)
+            case .DELETE:
+                result = try delete(ctx)
+            default:
+                result = .Next
+            }
+            return try after(ctx, result: result)
+            
+        case .Respond(let res):
+            return .Respond(res)
         }
     }
     func get(ctx: ContextBox) throws -> MiddlewareResult { return .Next }
