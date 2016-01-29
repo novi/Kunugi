e14e8e04e12bd08d291956714a104035d68c6723
Yusuke Ito <novi.mad@gmail.com>
2016-01-29 16:43:54 +0900

    change Package for latest swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Kunugi",
     dependencies: [
-		//.Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 2),
+	    .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 2),
     ]
 )
